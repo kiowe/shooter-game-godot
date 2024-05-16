df495bdb392085696ed63e673398e1d8550f4888
kiowe <ErickKrause2004@yandex.ru>
2024-05-16 23:57:50 +0300

    added crouch and needed checks; started working on the reticle

--- controllers/scripts/fps_controller.gd
+++ controllers/scripts/fps_controller.gd
@@ -1,12 +1,18 @@
 extends CharacterBody3D
 
-@export var SPEED : float = 5.0
+@export var SPEED_DEFAULT : float = 5.0
+@export var SPEED_CROUCH : float = 3.5
 @export var JUMP_VELOCITY : float = 4.5
+@export var TOGGLE_CROUCH : bool = true
+@export_range(5, 10, 0.1) var CROUCH_SPEED : float = 7.0
 @export var MOUSE_SENSITIVITY : float = 0.35
 @export var TILT_LOWER_LIMIT := deg_to_rad(-90.0)
 @export var TILT_UPPER_LIMIT := deg_to_rad(90.0)
 @export var CAMERA_CONTROLLER : Camera3D
+@export var ANIMATIONPLAYER : AnimationPlayer
+@export var CROUCH_SHAPECAST : Node3D
 
+var _speed : float
 var _mouse_input : bool = false
 var _mouse_rotation : Vector3
 var _rotation_input : float
@@ -14,11 +20,22 @@ var _tilt_input : float
 var _player_rotation : Vector3
 var _camera_rotation : Vector3
 
+var _is_crouching : bool = false
+
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _input(event):
 	if event.is_action_pressed("exit"):
 		get_tree().quit()
+	if event.is_action_pressed("crouch") and is_on_floor() and TOGGLE_CROUCH == true: # Press to crouch and uncrouch
+		toggle_crouch()
+	if event.is_action_pressed("crouch") and (_is_crouching == false) and is_on_floor() and TOGGLE_CROUCH == false: # Hold to crouch
+		crouching(true)
+	if event.is_action_released("crouch") and (_is_crouching == true) and TOGGLE_CROUCH == false: # Release to uncrouch
+		if CROUCH_SHAPECAST.is_colliding() == false:
+			crouching(false)
+		elif CROUCH_SHAPECAST.is_colliding() == true:
+			uncrouch_check()
 
 func _unhandled_input(event: InputEvent) -> void:
 	_mouse_input = event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
@@ -44,7 +61,14 @@ func _update_camera(delta):
 	_tilt_input = 0.0
 
 func _ready():
+	# Get mouse input
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	# Set default speed
+	_speed = SPEED_DEFAULT
+	
+	# Add crouch check shapecast collision exception for CharacterBody3D node
+	CROUCH_SHAPECAST.add_exception($".")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -54,7 +78,7 @@ func _physics_process(delta: float) -> void:
 	_update_camera(delta)
 	
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor() and _is_crouching == false:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -62,10 +86,43 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * _speed
+		velocity.z = direction.z * _speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, _speed)
+		velocity.z = move_toward(velocity.z, 0, _speed)
 
 	move_and_slide()
+
+func toggle_crouch():
+	if _is_crouching == true and CROUCH_SHAPECAST.is_colliding() == false:
+		crouching(false)
+	elif _is_crouching == false:
+		crouching(true)
+
+func uncrouch_check():
+	if CROUCH_SHAPECAST.is_colliding() == false:
+		crouching(false)
+	if CROUCH_SHAPECAST.is_colliding() == true:
+		await get_tree().create_timer(0.1).timeout
+		uncrouch_check()
+
+func crouching(state: bool):
+	match state:
+		true:
+			ANIMATIONPLAYER.play("Crouch", 0, CROUCH_SPEED)
+			set_movement_speed("crouching")
+		false:
+			ANIMATIONPLAYER.play("Crouch", 0, -CROUCH_SPEED, true)
+			set_movement_speed("default")
+
+func _on_animation_player_animation_started(anim_name: StringName) -> void:
+	if anim_name == "Crouch":
+		_is_crouching = !_is_crouching
+
+func set_movement_speed(state: String):
+	match state:
+		"default":
+			_speed = SPEED_DEFAULT
+		"crouching":
+			_speed = SPEED_CROUCH
